aa9ec75c4428fd98ea9ebc4429470768b7d75525
Samuel Marks <samuelmarks@gmail.com>
2018-11-12 14:55:42 +1100

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+offsh-postgres
+==============
+Bash scripts to automate Postgres.
+
+## user_db.bash
+
+Usage:
+
+    ./user_db.bash 'rest_user' 'rest_pass' 'localhost' 'rest_api_db'
---
+++ user_db.bash
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+function main() {
+  if [ -z "$1" ]; then
+    usage;
+  fi
+  local user="$1";
+  local password="$2";
+  local hostname="$3";
+  local db="$4";
+  create_user "$user";
+  create_db "$db" "$user";
+  printf -v RDBMS_URI "postgres://$user:$password@$hostname/$db"
+  export RDBMS_URI="$RDBMS_URI";
+  printf 'RDBMS_URI=%s\n' "'$RDBMS_URI'";
+}
+
+function usage() {
+  printf 'Usage: %s <user> <pasword> <host> <database>', "$0";
+}
+
+function create_db() {
+  local db="$1";
+  local user="$2";
+
+  if psql -lqt | cut -d \| -f 1 | grep -Fqw "$db"; then
+    return 0;
+  fi
+  
+  $(createdb "$db" --owner "$user");
+}
+
+function create_user() {
+  local user="$1";
+  if psql -t -c '\du' | grep -Fq "$user"; then
+    return 0;
+  fi
+  echo "User: '$user' does not exist";
+  $(createuser --superuser "$user");
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@";
+fi
